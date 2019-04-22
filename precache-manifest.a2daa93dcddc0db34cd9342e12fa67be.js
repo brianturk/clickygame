63ac0b60186de5ff2b6248b226d824adea4ad5b5
Brian Turk <bturksub@gmail.com>
2019-04-22 03:36:25 -0700

    Updates

---
+++ precache-manifest.a2daa93dcddc0db34cd9342e12fa67be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42e79bc98d6499aa86c84979581b420d",
+    "url": "/clickygame/static/media/Simpsonfont DEMO.42e79bc9.otf"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "29cf830f0b9d0ef9156e",
+    "url": "/clickygame/static/js/main.b6245368.chunk.js"
+  },
+  {
+    "revision": "30d5adb85e6653dd239b",
+    "url": "/clickygame/static/js/2.827c098e.chunk.js"
+  },
+  {
+    "revision": "29cf830f0b9d0ef9156e",
+    "url": "/clickygame/static/css/main.67062817.chunk.css"
+  },
+  {
+    "revision": "080ffbaf49e2ad9f3fb4875819882444",
+    "url": "/clickygame/index.html"
+  }
+];
