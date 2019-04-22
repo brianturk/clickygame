090ccd5dded1599c30dd2f40d1e9f1947c22c15c
Brian Turk <bturksub@gmail.com>
2019-04-22 03:32:19 -0700

    latest

--- src/App.js
+++ src/App.js
@@ -1,25 +1,140 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import SimpsonsCard from './components/SimpsonCard';
+import simpsons from "./simpsons.json";
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      score: 0,
+      topScore: 0,
+      simpsons: [],
+      selected: [],
+      bgColor: "",
+      newGame: false
+    };
+
+    this.playHaha = () => {
+      this.haha.play();
+    }
+
+    this.playSimpsonsIntro = () => {
+      this.simpsonsIntro.play();
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      simpsons: this.shuffleArray(simpsons)
+    });
+
+    this.playSimpsonsIntro()
+  }
+
+
+  handleClick = (id) => {
+
+    if (this.state.newGame) {
+      this.playSimpsonsIntro()
+
+      if (this.state.score > this.state.topScore) {
+        this.setState({
+          topScore: this.state.score
+        });
+      }
+
+      this.setState({
+        bgColor: "",
+        newGame: false,
+        selected: [],
+        simpsons: this.shuffleArray(simpsons),
+        score: 0
+      });
+    } else {
+      const newSelected = this.state.selected.filter(value => value !== id);
+
+      if (this.state.selected.length === newSelected.length) {
+        this.state.selected.push(id)
+        this.setState({
+          score: this.state.score + 1
+        });
+
+        this.setState({
+          simpsons: this.shuffleArray(simpsons)
+        });
+      } else {
+        //they messed up
+        this.setState({
+          bgColor: "red",
+          newGame: true
+        });
+
+        this.playHaha()
+
+      }
+    }
+  }
+
+  shuffleArray(arra1) {
+    var ctr = arra1.length, temp, index;
+
+    while (ctr > 0) {
+      index = Math.floor(Math.random() * ctr);
+      ctr--;
+      temp = arra1[ctr];
+      arra1[ctr] = arra1[index];
+      arra1[index] = temp;
+    }
+    return arra1;
+  }
+
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <nav className="navbar navbar-expand-lg navSimpson mb-3">
+          <div className="container">
+            <span className="navbar-brand navSimpsonBrand" href="#">Simpson's Clicky Game</span>
+
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <span className="scoreSimpsons">
+                  Score <span className="scoreBlack">{this.state.score}</span>Top Score<span className="scoreBlack">{this.state.topScore}</span>
+                </span>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              {this.state.simpsons.map(simpson => (
+                <SimpsonsCard
+                  handleClick={this.handleClick}
+                  image={simpson.image}
+                  id={simpson.id}
+                  key={simpson.id}
+                  name={simpson.name}
+                  bgColor={this.state.bgColor}
+                  newgame={this.state.newGame}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <audio ref={(haha) => { this.haha = haha; }}>
+          <source src="./assets/sound/haha.wav" type="audio/mpeg" >
+          </source>
+        </audio>
+        <audio ref={(simpsonsIntro) => { this.simpsonsIntro = simpsonsIntro; }}>
+          <source src="./assets/sound/theSimpsonsIntro.wav" type="audio/mpeg" >
+          </source>
+        </audio>
       </div>
     );
   }
---
+++ src/components/SimpsonCard/index.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class SimponsCard extends Component {
+  constructor(props) {
+    super(props);
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = props.handleClick.bind(this);
+
+    this.state = {
+      bgColor: "",
+      wrong: false,
+      newgame: false,
+      clicked: false
+    }
+  }
+
+  clickAnimation(id) {
+    console.log(this.state.bgColor);
+
+    if (!this.state.wrong) {
+      this.setState({
+        bgColor: "#FFD90F",
+        clicked: true
+      })
+      setTimeout(function () {
+        this.setState({
+          bgColor: "#6CADDC"
+        })
+        this.handleClick(id)
+      }.bind(this), 300);
+    } else {
+      this.handleClick(id)
+    }
+  }
+
+  overColor() {
+    if (!this.state.wrong) {
+      this.setState({
+        bgColor: "green"
+      })
+    }
+  }
+
+  leaveColor() {
+    if (!this.state.wrong) {
+      this.setState({
+        bgColor: ""
+      })
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    // Typical usage (don't forget to compare props):
+    // this.fetchData(this.props.userID);
+    if ((prevProps.bgColor === "red") && (this.state.wrong) && (this.props.bgColor === "")) {
+      this.setState({
+        bgColor: "",
+        wrong: false
+      })
+    }
+    if ((this.props.bgColor === "red") && (!this.state.wrong)) {
+      if (this.state.clicked) {
+        this.setState({
+          bgColor: this.props.bgColor,
+          wrong: true,
+          clicked: false
+        })
+      } else {
+        this.setState({
+          wrong: true
+        })
+      }
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="card" onClick={() => this.clickAnimation(this.props.id)} onMouseOver={() => this.overColor()} onMouseLeave={() => this.leaveColor()}>
+        <div style={{ backgroundColor: this.state.bgColor }} className="img-container">
+          <img alt={this.props.name} src={this.props.image} />
+        </div>
+      </div>
+    );
+  }
+
+}
+
+
+export default SimponsCard;
